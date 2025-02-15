5937e3e17ed3acb164df4819c0892a4a85a420ff
Vadim <_s_e_t__@ukr.net>
2025-02-15 12:11:47 +0200

    add some styles

--- components/ExpensesOutput/ExpenseItem.tsx
+++ components/ExpensesOutput/ExpenseItem.tsx
@@ -1,5 +1,7 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
+
 import { Expense } from "../../types/expense";
+import { GlobalStyles } from "../../constanst/styles";
 
 interface ExpenseItemProps {
   item: Expense;
@@ -8,12 +10,14 @@ interface ExpenseItemProps {
 const ExpenseItem = ({ item }: ExpenseItemProps) => {
   return (
     <Pressable>
-      <View>
+      <View style={styles.itemContainer}>
         <View>
-          <Text>{item.description}</Text>
-          <Text>{item.date}</Text>
+          <Text style={[styles.textBase,styles.description]}>{item.description}</Text>
+          <Text style={styles.textBase}>{item.date}</Text>
         </View>
-        <Text>{item.amount}</Text>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{item.amount}</Text>
+        </View>
       </View>
     </Pressable>
   );
@@ -21,4 +25,35 @@ const ExpenseItem = ({ item }: ExpenseItemProps) => {
 
 export default ExpenseItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  itemContainer: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    width:80,
+  },
+  amount: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
+  },
+});
